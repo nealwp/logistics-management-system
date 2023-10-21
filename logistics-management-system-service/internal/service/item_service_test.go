5b4782ee52e480925b9e7fd5a913ab0805f52f02
nealwp <mrprestonneal@gmail.com>
2023-10-21 16:03:15 -0700

    add id validation test

--- logistics-management-system-service/internal/service/item_service_test.go
+++ logistics-management-system-service/internal/service/item_service_test.go
@@ -19,6 +19,21 @@ func (m *MockDatabase) InsertItem(item *domain.Item) error {
 
 func TestAddItem(t *testing.T) {
 
+    t.Run("valid item does not return error", func(t *testing.T) {
+        mockDb := new(MockDatabase)
+        mockDb.On("InsertItem", mock.Anything).Return(nil)
+        service := NewItemService(mockDb)
+        item := &domain.Item{ID: "123456789", Name: "Sample Item", UnitPrice: 1234}
+
+        err := service.AddItem(item)
+
+        if err != nil {
+            t.Errorf("Did not expect error, but got: %v", err)
+        }
+
+        mockDb.AssertExpectations(t)
+    })
+
     t.Run("empty name returns error", func(t *testing.T) {
         mockDb := new(MockDatabase)
         service := NewItemService(mockDb)
@@ -31,22 +46,6 @@ func TestAddItem(t *testing.T) {
         }
     })
 
-    t.Run("valid name does not return error", func(t *testing.T) {
-        mockDb := new(MockDatabase)
-        mockDb.On("InsertItem", mock.Anything).Return(nil)
-        service := NewItemService(mockDb)
-        item := &domain.Item{ID: "123456789", Name: "Sample Item", UnitPrice: 1234}
-
-        err := service.AddItem(item)
-
-        if err != nil {
-            t.Errorf("Did not expect error, but got: %v", err)
-        }
-
-        mockDb.AssertExpectations(t)
-    })
-
-    
     t.Run("zero unit price returns error", func(t *testing.T) {
         mockDb := new(MockDatabase)
         service := NewItemService(mockDb)
@@ -59,22 +58,18 @@ func TestAddItem(t *testing.T) {
         }
     })
 
-    t.Run("non-zero unit price does not return error", func(t *testing.T) {
+    t.Run("empty id returns error", func(t *testing.T) {
         mockDb := new(MockDatabase)
         service := NewItemService(mockDb)
-        mockDb.On("InsertItem", mock.Anything).Return(nil)
-        item := &domain.Item{ID: "123456789", Name: "Sample Item", UnitPrice: 1234}
+        item := &domain.Item{ID: "", Name: "Sample Item", UnitPrice: 0}
 
         err := service.AddItem(item)
 
-        if err != nil {
-            t.Errorf("Did not expect error, but got: %v", err)
+        if err == nil {
+            t.Error("expected error, but got none")
         }
-
-        mockDb.AssertExpectations(t)
     })
 
-    
     t.Run("db fail to write returns error", func(t *testing.T) {
         mockDb := new(MockDatabase)
         service := NewItemService(mockDb)
