56371dbcc0f9659342ae9381165b2f291601fc6c
nealwp <mrprestonneal@gmail.com>
2023-10-23 20:24:36 -0700

    navs in ngFor

--- logistics-management-system-ui/src/app/navbar/navbar.component.ts
+++ logistics-management-system-ui/src/app/navbar/navbar.component.ts
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+    navOptions = [
+        {
+            title: 'Items',
+            id: 'itemDropdown',
+            actions: [
+                { label: 'Add Item', route: '/add-item' },
+                { label: 'Item List', route: '/list-items' }
+            ]
+        },
+        {
+            title: 'Allowances',
+            id: 'allowanceDropdown',
+            actions: [
+                { label: 'Add Allowance', route: '/add-allowance' },
+                { label: 'Allowance List', route: '/list-allowances' }
+            ]
+        },
+        {
+            title: 'Inventory',
+            id: 'inventoryDropdown',
+            actions: [
+                { label: 'Create Location', route: '/add-location' },
+                { label: 'Re-order Stock', route: '/order-stock' },
+                { label: 'Schedule Inventory Count', route: '/schedule-inventory' },
+                { label: 'Post Inventory Counts', route: '/post-inventory' },
+                { label: 'Offload Excess Stock', route: '/offload' },
+            ]
+        },
+        {
+            title: 'Orders',
+            id: 'ordersDropdown',
+            actions: [
+                { label: 'New Order', route: '/new-order' },
+                { label: 'Update Order Status', route: '/order-status' },
+                { label: 'Active Orders', route: '/orders' },
+                { label: 'Order History', route: '/order-history' },
+            ]
+        }
+    ]
+
 }
